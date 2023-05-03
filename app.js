8c82100ff2cc94a9d62306b24fe5cdcd2436341d
Amirreza Ghobadian <ghobadiyanamirreza@gmail.com>
2023-05-04 00:41:23 +0430

    Q1

---
+++ app.js
@@ -0,0 +1,24 @@
+//1
+document.querySelector('#age-table');
+
+//2
+document.querySelectorAll('#age-table label');
+
+
+//3
+document.querySelector('#age-table td:first-child');
+
+
+
+//4
+document.querySelector('form[name="search"]');
+
+
+
+//5
+document.querySelector('form[name="search"] input:first-of-type');
+
+
+
+//6
+document.querySelector('form[name="search"] input:last-of-type');
