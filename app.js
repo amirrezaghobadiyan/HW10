3a01595b4f97f2573d42358b9182ced3d6a97e7d
Amirreza Ghobadian <ghobadiyanamirreza@gmail.com>
2023-05-04 00:44:45 +0430

    Q5

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
-function createTree(container, data) {
-    const list = document.createElement('ul');
-
-    for (let key in data) {
-        const item = document.createElement('li');
-        item.textContent = key;
-
-        if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
-            // If the value is an object with children, recursively create a nested list
-            const nestedList = createTree(document.createElement('div'), data[key]);
-            item.appendChild(nestedList);
-        }
-
-        list.appendChild(item);
+let elem = document.getElementById("elem");
+function clear(elem) {
+    while (elem.firstChild) {
+        elem.removeChild(elem.firstChild);
     }
-
-    container.appendChild(list);
-    return list;
 }
-const container = document.getElementById('container');
-const data = {
-    "Fish": {
-        "trout": {},
-        "salmon": {}
-    },
-    "Tree": {
-        "Huge": {
-            "sequoia": {},
-            "oak": {}
-        },
-        "Flowering": {
-            "apple tree": {},
-            "magnolia": {}
-        }
-    }
-};
-createTree(container, data);
-
+// clear(elem);
