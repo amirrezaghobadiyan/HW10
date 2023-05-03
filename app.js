83072dc7f9641015b99d90e8633140e12e460885
Amirreza Ghobadian <ghobadiyanamirreza@gmail.com>
2023-05-04 00:48:12 +0430

    Q9

--- app.js
+++
@@ -1,8 +0,0 @@
-const closeButtons = document.querySelectorAll('.close-button');
-
-for (let i = 0; i < closeButtons.length; i++) {
-    const button = closeButtons[i];
-    button.addEventListener('click', () => {
-        button.parentNode.remove();
-    });
-}
